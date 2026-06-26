10c75c4ea8510e4bd7f13f08f00d8a59f909ed14
Cristian Lucchesi <cristian.lucchesi@iit.cnr.it>
2026-06-26 19:01:13 +0200

    Aggiunti test di integrazione per `syncOfficeMonth`, introdotta gestione batch con `EntityManager`, aggiornate dipendenze e incrementata versione a 0.3.0.

--- src/test/java/it/cnr/iit/epas/timesheet/ugovpj/SpringCommonTest.java
+++ src/test/java/it/cnr/iit/epas/timesheet/ugovpj/SpringCommonTest.java
@@ -8,7 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ActiveProfiles;
 
+import it.cnr.iit.epas.timesheet.ugovpj.client.EpasClient;
+import it.cnr.iit.epas.timesheet.ugovpj.repo.PersonTimeDetailRepo;
 import it.cnr.iit.epas.timesheet.ugovpj.service.TimeDetailTypeService;
+import jakarta.inject.Inject;
 
 @ActiveProfiles(value = {"test", "h2"})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -17,9 +20,16 @@ public class SpringCommonTest {
   @MockitoBean
   TimeDetailTypeService timeDetailTypeService;
 
+  @MockitoBean
+  protected EpasClient epasClient;
+
+  @Inject
+  PersonTimeDetailRepo repo;
+
   @BeforeEach
   public void setUp() {
     when(timeDetailTypeService.timeDetailTypes()).thenReturn(Sets.set("N", "F", "T", "X"));
+    repo.deleteAll();
   }
 
 }
